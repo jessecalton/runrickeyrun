342ec18ef84fcd0c11ff3b310014c0fcdcf012ff
Jesse Calton <jessecalton@gmail.com>
2017-07-18 18:48:19 -0700

    changes font color to goldenrod

--- content.js
+++ content.js
@@ -2,7 +2,8 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
     document.body.style.backgroundColor = "green";
-    document.body.innerHTML = document.body.innerHTML.replace(/\s[I]\s/g, " Rickey Henderson ")
+    document.body.innerHTML = document.body.innerHTML.replace(/\s[I]\s/g, " Rickey Henderson ");
+    document.body.style.color = "goldenrod";
     }
   }
 ); 
