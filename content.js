6fc4375c5d123619a19e8aaf034d98afb3f80943
Jesse Calton <jessecalton@gmail.com>
2017-07-18 19:01:53 -0700

    adds more regex checkers

--- content.js
+++ content.js
@@ -3,6 +3,11 @@ chrome.runtime.onMessage.addListener(
     if( request.message === "clicked_browser_action" ) {
     document.body.style.backgroundColor = "green";
     document.body.innerHTML = document.body.innerHTML.replace(/\s[I]\s/g, " Rickey Henderson ");
+    document.body.innerHTML = document.body.innerHTML.replace(/[I]\s/g, " Rickey Henderson ");
+    document.body.innerHTML = document.body.innerHTML.replace(/\s\me\s/g, " Rickey Henderson ");
+    document.body.innerHTML = document.body.innerHTML.replace(/\s\my\s/g, " Rickey Henderson's ");
+    document.body.innerHTML = document.body.innerHTML.replace(/\My\s/g, "Rickey Henderson's ");
+    document.body.innerHTML = document.body.innerHTML.replace(/\I\’\m/g, "Rickey Henderson's");
     document.body.style.color = "goldenrod";
     }
   }
